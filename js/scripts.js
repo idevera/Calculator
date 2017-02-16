f4ea0551be0d62ed1757eca0e24472e404f2da98
Irene DeVera <Guest@epicodus-31.local>
2017-02-15 19:46:00 -0800

    Add more forms

--- js/scripts.js
+++ js/scripts.js
@@ -17,14 +17,40 @@ var divide = function(number1, number2) {
 };
 
 //Everything below this line is user interface (front-end)
-
+//Addition of two numbers
 $(document).ready(function() {
   $("form#add").submit(function(event) {
     event.preventDefault();
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
     var result = add(number1, number2);
-    $("#output").text(result);
+    $("#addoutput").text(result);
+  });
 
+//Subtraction of two numbers
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#subtract1").val());
+    var number2 = parseInt($("#subtract2").val());
+    var result = subtract(number1,number2);
+    $("#subtract-output").text(result);
+  });
+
+//Multiplication of two numbers
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#multiply1").val());
+    var number2 = parseInt($("#multiply2").val());
+    var result = multiply(number1, number2);
+    $("#multiply-output").text(result);
+  });
+
+//Division of two numbers
+  $("form#div").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#div1").val());
+    var number2 = parseInt($("#div2").val());
+    var result = divide(number1, number2);
+    $("#div-output").text(result);
   });
 });
